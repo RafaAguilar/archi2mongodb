54b47813ef63425ba5745bf5265e622a7d0380c3
Rafael Aguilar <rafael.aguilar@centralmadeirense.com.ve>
2015-04-08 15:03:32 -0430

    fix: Adding Mongo Support

---
+++ archimate2mongodb/pkg/mongo/client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.4
+
+"""TODO"""
+from pymongo import MongoClient
+
+
+class MongoClient:
+    
+    """TODO"""    
+    def __new__(self, host=None, port=None, options={}):
+        self.client=None
+        try:
+            if host and port:
+                self.client = MongoClient(host,port) 
+            else:   
+                self.client = MongoClient()
+            self.initialize_database()            
+        except Exception:
+            print "error"
+        
+        return self.client    
+     
+    def get_client(self):
+        return self.client
+    
+    def initialize_database(self):
+        self.db = self.client.archimate
+        self.archi_collection = db.elements
+                                         
+    def find_one(self):
+        print(self.archi_collection.find_one())
