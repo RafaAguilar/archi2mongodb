63123c35242d6e95b1cb7ad8814ffe37d691660a
Rafael Aguilar <rafael.aguilar@centralmadeirense.com.ve>
2015-04-08 15:03:32 -0430

    Adding Mongo Support

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ A Python app that parse Archimate Exchange File to a MongoDB
 Modules needed:
 
  - xmltodict
+ - pymongo (>= 3.0rc1) # Click [here][pymongo3] for installing instructions (in spanish)
+ 
+ [pymongo3]:http://rafaaguilar.github.io/entradas/mongo-30-en-rhel-5/#instalando-pymongo-30rc1-python-driver-para-mongodb-30
  
--- archimate2mongodb/main.py
+++ archimate2mongodb/main.py
@@ -5,29 +5,30 @@ from pkg.relations.relation import *
 from pkg.elements.element import *
 from pkg.basics.model import *
 from pkg.basics.relations import *
+from pkg.mongo.client import *
 
 if __name__ == '__main__':
     
-    archi_model = load_model_to_dict('../examples/file_format_archimate.xml')
+    mongoclient = MongoClient('cmpmo01.cm.cm.ve',27017)
+    mongoclient.find_one()
     
-    property_defs=get_property_defs(archi_model)
-    
-    relationships=[]
-    for relation in get_model_child(archi_model, Model.Relationships, Model.Relation):
-        relationships.append(Relation(relation))
-        
-    elements=[]
-    for element in get_model_child(archi_model,Model.Elements,Model.Element):
-        element_instance = Element(element)  
-        for r in relationships:
-            if element_instance.id() == r.source_id():
-                element_instance.add_relation(r)
-        element_instance.set_properties(get_element_props(element, property_defs))
-        elements.append(element_instance)
-        
-    elements[0].pretty_print()
-    elements[30].pretty_print()
-    elements[31].pretty_print()
-  
-    
-    #print(get_element_props(get_model_child(archi_model,Model.Elements,Model.Element)[30],property_defs))
+    # archi_model = load_model_to_dict('../examples/file_format_archimate.xml')
+    # 
+    # property_defs=get_property_defs(archi_model)
+    # 
+    # relationships=[]
+    # for relation in get_model_child(archi_model, Model.Relationships, Model.Relation):
+    #     relationships.append(Relation(relation))
+    #     
+    # elements=[]
+    # for element in get_model_child(archi_model,Model.Elements,Model.Element):
+    #     element_instance = Element(element)  
+    #     for r in relationships:
+    #         if element_instance.id() == r.source_id():
+    #             element_instance.add_relation(r)
+    #     element_instance.set_properties(get_element_props(element, property_defs))
+    #     elements.append(element_instance)
+    #     
+    # elements[0].pretty_print()
+    # elements[30].pretty_print()
+    # elements[31].pretty_print()
