cc57331670f8609484176f445408c7b43ca0eea0
Rafael Aguilar <rafael.aguilar@centralmadeirense.com.ve>
2015-04-13 14:21:58 -0430

    Main example to test views support

--- archimate2mongodb/main.py
+++ archimate2mongodb/main.py
@@ -10,26 +10,28 @@ from pkg.mongo.util import *
 
 if __name__ == '__main__':
     
-    mongoclient = MongoUtil()  
-    
+mongoclient = MongoUtil()  
+
     archi_model = load_model_to_dict('../examples/file_format_archimate.xml')
     
     property_defs=get_property_defs(archi_model)
     
-    relationships=[]
-    for relation in get_model_child(archi_model, Model.Relationships, Model.Relation):
-        relationships.append(Relation(relation))
+    # relationships=[]
+    # for relation in get_model_child(archi_model, Model.Relationships, Model.Relation):
+    #     relationships.append(Relation(relation))
         
-    elements=[]
-    for element in get_model_child(archi_model,Model.Elements,Model.Element):
-        element_instance = Element(element)  
-        for r in relationships:
-            if element_instance.id() == r.source_id():
-                element_instance.add_relation(r)
-        element_instance.set_properties(get_element_props(element, property_defs))
-        elements.append(element_instance)
-        mongoclient.insert_element(element_instance)
+    # elements=[]
+    # for element in get_model_child(archi_model,Model.Elements,Model.Element):
+    #     element_instance = Element(element)  
+    #     for r in relationships:
+    #         if element_instance.id() == r.source_id():
+    #             element_instance.add_relation(r)
+    #     element_instance.set_properties(get_element_props(element, property_defs))
+    #     elements.append(element_instance)
+        #mongoclient.insert_element(element_instance)
         
     views=[]
     for view in get_model_child(archi_model, Model.Views, Model.View):
         views.append(View(view))
+                
+    print(views[0].details())
